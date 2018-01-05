8ed16647bfdae54d28b79f06ad113f0d0f0ced7d
hadrianl <137150224@qq.com>
2018-01-05 14:54:29 +0800

    增加了日志记录

--- order_detail_sub.py
+++ order_detail_sub.py
@@ -14,63 +14,53 @@ import pandas as pd
 from datetime import datetime
 import time
 from colorama import init, Fore, Back, Style
+import logging.config
+import sys
 
-
+logging.config.fileConfig('log.conf')
+logger = logging.getLogger('order_subscribe')
 init(autoreset=True)
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-ip = input('输入IP：')
-# ip = ip if ip else '*'
-port = input('请输入端口号(默认为5000):')
-port = port if port else '5000'
-socket.connect(f"tcp://{ip}:{port}")
-socket.setsockopt_string(zmq.SUBSCRIBE, '')
-orders_dict = {}
+try:
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    ip = input('输入IP：')
+    port = input('请输入端口号(默认为5000):')
+    port = port if port else '5000'
+    socket.connect(f"tcp://{ip}:{port}")
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')
+    orders_dict = {}
+except Exception:
+    logger.exception(f'初始化连接失败---tcp://{ip}:{port}')
+    sys.exit()
 
-
-def order_type_mapping(order_comment):
-    if 'cancelled' in order_comment:
-        return '取消订单'
-    elif '[sl]' in order_comment:
-        return '止损'
-    elif '[tp]' in order_comment:
-        return '止盈'
-    else:
-        return '平仓'
-
-
-print(f'订阅订单变化成功.................{ip}:{port}')
-while True:
-    try:
+logger.info(f'订阅订单变化成功  from  {ip}:{port}')
+try:
+    while True:
         new_order = socket.recv_pyobj()
-        # todo LOG
-        orders_dict.update(new_order)
+        orders_dict.update({new_order.get('Ticket'):new_order})
         if new_order.get('Status') == 1:
-            text = f"""{Fore.WHITE}{str(datetime.now()):}{Fore.RESET}
-{Back.BLUE}#{new_order.get('Ticket')}---开仓{Back.RESET}：{new_order}
-------------------------------------------------------------------------------"""
-        elif new_order.get('Status') == 2:
-            text = f"""{Fore.WHITE}{str(datetime.now()):}{Fore.RESET}
-{Back.YELLOW}#{new_order.get('Ticket')}---{order_type_mapping(new_order.get('Comment'))}{Back.RESET}：{new_order}
-------------------------------------------------------------------------------"""
+            log_info = f'账户:{new_order.get("Account_ID")}--于{new_order.get("OpenTime")}开仓--{new_order.get("Symbol")}@{new_order.get("OpenPrice")}-#{new_order.get("Ticket")}'
         elif new_order.get('Status') == 0:
-            text = f"""{Fore.WHITE}{str(datetime.now()):}{Fore.RESET}
-{Back.BLUE}#{new_order.get('Ticket')}---挂单{Back.RESET}：{new_order}
-------------------------------------------------------------------------------"""
+            log_info = f'账户:{new_order.get("Account_ID")}--于{new_order.get("OpenTime")}挂单--{new_order.get("Symbol")}@{new_order.get("OpenPrice")}-#{new_order.get("Ticket")}'
         elif new_order.get('Status') == -1:
-            text = f"""{Fore.WHITE}{str(datetime.now()):}{Fore.RESET}
-{Back.LIGHTWHITE_EX}#{new_order.get('Ticket')}---取消订单{Back.RESET}：{new_order}
-------------------------------------------------------------------------------"""
+            log_info = f'账户:{new_order.get("Account_ID")}--于{new_order.get("OpenTime")}取消挂单--{new_order.get("Symbol")}@{new_order.get("OpenPrice")}-#{new_order.get("Ticket")}'
+        elif new_order.get('Status') == 2:
+            tp = new_order.get('Comment').find('[tp]') != -1
+            sl = new_order.get('Comment').find('[sl]') != -1
+            has_comment = bool(new_order.get('Comment'))
+            close_type = {0: '平仓', 1: new_order.get('Comment'), 2: '止损', 3: '止盈', 4: new_order.get('Comment')}.get(((tp << 1) + sl + 1) * has_comment)
+            log_info = f'账户:{new_order.get("Account_ID")}--于{new_order.get("OpenTime")}{close_type}--{new_order.get("Symbol")}@{new_order.get("OpenPrice")}-#{new_order.get("Ticket")}'
         else:
-            text = f"""{Fore.WHITE}{str(datetime.now()):}{Fore.RESET}
-{Back.YELLOW}#{new_order.get('Ticket')}---{Back.RESET}：{new_order}
-------------------------------------------------------------------------------"""
-
-        print(text)
-    except Exception as e:
-        print('关闭监测CLIENT！', e)
-        socket.close()
-        df = pd.DataFrame(orders_dict)
-        df.to_excel('orders.xlsx')
-        time.sleep(10)
-        break
+            log_info = f'Status:{new_order.get("Status")}'
+        logger.info(log_info)
+except Exception as e:
+    logger.exception('订阅接收异常')
+    socket.close()
+    df = pd.DataFrame([nd for _, nd in orders_dict.items()])
+    df.to_excel('orders.xlsx')
+except SystemExit:
+    print('正向关闭订阅.........')
+    socket.close()
+    df = pd.DataFrame([nd for _, nd in orders_dict.items()])
+    df.to_excel('orders.xlsx')
+    time.sleep(3)
